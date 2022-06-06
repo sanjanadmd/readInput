f9e6711f6e9a289b1bfcb27a383742dbf399cfc0
sanjanadondapati <99525594+sanjanadmd@users.noreply.github.com>
2022-06-06 16:56:40 +0530

    Seperated User entity from userDetails

--- fetchUserDetails.js
+++ fetchUserDetails.js
@@ -1,60 +1,8 @@
 const fs = require('fs');
 
+const { User } = require("./user.js");
+
 process.stdin.setEncoding('utf8');
-
-class User {
-  constructor() {
-    this.user = {};
-  }
-
-  #validateName(name) {
-    if (name.length > 4) {
-      return !isFinite(name);
-    }
-    return false;
-  }
-  addName(name) {
-    if (this.#validateName(name)) {
-      this.user.name = name;
-      return true;
-    }
-    return false;
-  }
-  #validateYear(year) {
-    return year.length === 4 && isFinite(year);
-  }
-  #validateMonth(month) {
-    return month.length === 2 && isFinite(month);
-  }
-  #validateDate(date) {
-    return date.length === 2 && isFinite(date);
-  }
-  #validateDOB(dob) {
-    const [year, month, date] = dob.split('-');
-    return this.#validateYear(year) && this.#validateMonth(month) && this.#validateDate(date);
-  }
-  addDOB(dob) {
-    if (this.#validateDOB(dob)) {
-      this.user.dob = dob;
-      return true;
-    }
-    return false;
-  }
-  #validateHobbies(hobbies) {
-    return hobbies.length > 0 && hobbies[0] !== '';
-  }
-  addHobbies(hobbiesString) {
-    const hobbies = hobbiesString.split(',');
-    if (this.#validateHobbies(hobbies)) {
-      this.user.hobbies = hobbies;
-      return true;
-    }
-    return false;
-  }
-  registerDetails() {
-    return JSON.stringify(this.user);
-  }
-}
 
 const readInput = (queries) => {
   let input = '';
