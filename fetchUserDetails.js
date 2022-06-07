ba1d6c03d64eb3b24b9089063c190eba23350da9
sanjanadondapati <99525594+sanjanadmd@users.noreply.github.com>
2022-06-07 10:04:32 +0530

    Seperated validations from user

--- fetchUserDetails.js
+++ fetchUserDetails.js
@@ -3,18 +3,45 @@ const fs = require('fs');
 const { User } = require("./user.js");
 
 process.stdin.setEncoding('utf8');
+const validateName = (name) => {
+  if (name.length > 4) {
+    return !isFinite(name);
+  }
+  return false;
+}
 
+const validateNumber = (number, digits) => {
+  return number?.length === digits && isFinite(number);
+}
+
+const validateDOB = (dob) => {
+  const [year, month, date] = dob.split('-');
+  return validateNumber(year, 4) && validateNumber(month, 2) && validateNumber(date, 2);
+}
+
+const validateHobbies = (hobbies) => {
+  return hobbies.length > 0 && hobbies[0] !== '';
+}
+
+const validatePhoneNumber = (number) => {
+  return number.length === 10 && isFinite(number);
+}
+
+const validateAddress = (address) => {
+  return address !== '';
+}
 const readInput = (queries, cb) => {
   let index = 0;
   console.log(queries[index].query);
   process.stdin.on('data', (chunk) => {
-    const [input] = chunk.split('\n');
-    if (queries[index].answer(input)) {
+    const input = chunk.split('\n')[0];
+    if (queries[index].validate(input)) {
+      queries[index].answer(input);
       index++;
     }
     if (queries.length === index) {
+      fs.writeFileSync('form.json', cb(), 'utf8');
       console.log('Thank you');
-      fs.writeFileSync('user.json', cb(), 'utf8');
       process.exit();
     }
     console.log(queries[index].query);
@@ -26,22 +53,34 @@ const getDetails = () => {
   const user = new User();
   const queries = [
     {
-      query: 'Please enter your name:', answer: (name) => user.addName(name)
+      query: 'Please enter your name:',
+      answer: (name) => user.addName(name),
+      validate: validateName
     },
     {
-      query: 'Please enter your DOB(yyyy-mm-dd)', answer: (dob) => user.addDOB(dob)
+      query: 'Please enter your DOB(yyyy-mm-dd)',
+      answer: (dob) => user.addDOB(dob),
+      validate: validateDOB
     },
     {
-      query: 'Please enter your hobbies', answer: (hobbies) => user.addHobbies(hobbies)
+      query: 'Please enter your hobbies',
+      answer: (hobbies) => user.addHobbies(hobbies),
+      validate: validateHobbies
     },
     {
-      query: 'Enter phone number', answer: (number) => user.addPhoneNumber(number)
+      query: 'Enter phone number',
+      answer: (number) => user.addPhoneNumber(number),
+      validate: validatePhoneNumber
     },
     {
-      query: 'Enter address line 1', answer: (address) => user.addAddress(address)
+      query: 'Enter address line 1',
+      answer: (address) => user.addAddress(address),
+      validate: validateAddress
     },
     {
-      query: 'Enter address line 2', answer: (address) => user.addAddress(address)
+      query: 'Enter address line 2',
+      answer: (address) => user.addAddress(address),
+      validate: validateAddress
     },
   ];
   readInput(queries, () => user.registerDetails());
--- user.js
+++ user.js
@@ -1,68 +1,20 @@
-const validateYear = (year) => {
-  return year?.length === 4 && isFinite(year);
-}
-const validateMonth = (month) => {
-  return month?.length === 2 && isFinite(month);
-}
-const validateDate = (date) => {
-  return date?.length === 2 && isFinite(date);
-}
-
-const validateDOB = (dob) => {
-  const [year, month, date] = dob.split('-');
-  return validateYear(year) && validateMonth(month) && validateDate(date);
-}
-
 class User {
   constructor() {
     this.user = {};
     this.user.address = [];
   }
-
-  #validateName(name) {
-    if (name.length > 4) {
-      return !isFinite(name);
-    }
-    return false;
-  }
   addName(name) {
-    if (this.#validateName(name)) {
-      this.user.name = name;
-      return true;
-    }
-    return false;
+    this.user.name = name;
   }
   addDOB(dob) {
-    if (validateDOB(dob)) {
-      this.user.dob = dob;
-      return true;
-    }
-    return false;
-  }
-  #validateHobbies(hobbies) {
-    return hobbies.length > 0 && hobbies[0] !== '';
+    this.user.dob = dob;
   }
   addHobbies(hobbiesString) {
     const hobbies = hobbiesString.split(',');
-    if (this.#validateHobbies(hobbies)) {
-      this.user.hobbies = hobbies;
-      return true;
-    }
-    return false;
-  }
-
-  #validatePhoneNumber(number) {
-    return number.length === 10 && isFinite(number);
+    this.user.hobbies = hobbies;
   }
   addPhoneNumber(number) {
-    if (this.#validatePhoneNumber(number)) {
-      this.user.ph_no = number;
-      return true;
-    }
-    return false;
-  }
-  #validateAddress(address) {
-    return address !== '';
+    this.user.ph_no = number;
   }
 
   setAddress() {
@@ -71,12 +23,8 @@ class User {
     }
   }
   addAddress(address) {
-    if (this.#validateAddress(address)) {
-      this.setAddress();
-      this.user.address = (this.user.address + '\n' + address).trim();
-      return true;
-    }
-    return false;
+    this.setAddress();
+    this.user.address = (this.user.address + '\n' + address).trim();
   }
   registerDetails() {
     return JSON.stringify(this.user);
